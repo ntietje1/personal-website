e8b2317deb82086fa1f6c46ea724505081e0bf0b
Nick <tietje.n@northeastern.edu>
2024-09-04 11:55:46 -0400

    attempt fix hover

--- app/components/link.tsx
+++ app/components/link.tsx
@@ -7,16 +7,18 @@ interface LinkProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
 export function ArrowIcon() {
   return (
     <svg
-      width="12"
-      height="12"
-      viewBox="0 0 12 12"
-      fill="none"
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="currentColor"
       xmlns="http://www.w3.org/2000/svg"
-      className="inline-block h-2 w-2 shrink-0 transition-transform group-hover:-translate-y-0.5 group-hover:translate-x-0.5 group-focus-visible:-translate-y-0.5 group-focus-visible:translate-x-0.5 motion-reduce:transition-none ml-1 translate-y-px"
+      className="inline-block h-3 w-3 shrink-0 transition-transform group-hover/link:-translate-y-0.5 group-hover/link:translate-x-0.5 group-focus-visible/link:-translate-y-0.5 group-focus-visible/link:translate-x-0.5 motion-reduce:transition-none ml-1 translate-y-px"
+      aria-hidden="true"
     >
       <path
-        d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
-        fill="currentColor"
+        fillRule="evenodd"
+        d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+        clipRule="evenodd"
       />
     </svg>
   );
@@ -28,7 +30,7 @@ export const LinkWithArrowNoAnchor: React.FC<LinkProps> = ({
 }) => {
   return (
     <div
-      className={`inline-flex items-baseline leading-tight text-slate-200 group-hover:text-purple-400 focus-visible:text-purple-300 group text-base ${className}`}
+      className={`inline-flex items-baseline leading-tight text-slate-200 group-hover:text-purple-400 focus-visible:text-purple-300 text-base ${className}`}
     >
       <div className="pr-1">{children}</div>
       <ArrowIcon />
@@ -40,7 +42,7 @@ export const LinkWithArrow: React.FC<LinkProps> = ({ children, ...props }) => {
   return (
     <a
       {...props}
-      className="inline-flex items-baseline leading-tight text-slate-200 group-hover:text-purple-400 focus-visible:text-purple-300 group text-base"
+      className="inline-flex items-baseline leading-tight text-slate-200 group-hover:text-purple-400 focus-visible:text-purple-300 text-base"
     >
       <div className="pr-1">{children}</div>
       <ArrowIcon />
@@ -52,7 +54,7 @@ export const Link: React.FC<LinkProps> = ({ children, ...props }) => {
   return (
     <a
       {...props}
-      className="inline-flex items-baseline leading-tight text-slate-200 hover:text-purple-300 focus-visible:text-purple-300 group text-base"
+      className="inline-flex items-baseline leading-tight text-slate-200 hover:text-purple-300 focus-visible:text-purple-300 text-base"
     >
       {children}
     </a>
