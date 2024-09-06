31b8c22d1daa7038b0fbba35981142e1d617d764
Nicholas Tietje <tietje.n@northeastern.edu>
2024-09-05 22:08:15 -0400

    remove unused imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,8 @@
 import "./global.css";
 import type { Metadata } from "next";
-import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
-import NavMenu from "./components/nav";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import Footer from "./components/footer";
 import { baseUrl } from "./sitemap";
 
 export const metadata: Metadata = {
@@ -21,48 +18,51 @@ export const metadata: Metadata = {
     url: baseUrl,
     siteName: "Nick Tietje's Portfolio",
     locale: "en_US",
-        type: "website",
-      },
-      robots: {
-        index: true,
-        follow: true,
-        googleBot: {
-          index: true,
-          follow: true,
-          "max-video-preview": -1,
-          "max-image-preview": "large",
-          "max-snippet": -1,
-        },
-      },
-    };
-    
-    const cx = (...classes) => classes.filter(Boolean).join(" ");
-    
-    export default function RootLayout({
-      children,
-    }: {
-      children: React.ReactNode;
-    }) {
-      return (
-        <html
-          lang="en"
-          className={cx(
-            "text-black bg-white dark:text-white dark:bg-black",
-            GeistMono.variable,
-            GeistMono.variable
-          )}
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+};
+
+const cx = (...classes) => classes.filter(Boolean).join(" ");
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html
+      lang="en"
+      className={cx(
+        "text-black bg-white dark:text-white dark:bg-black",
+        GeistMono.variable,
+        GeistMono.variable
+      )}
+    >
+      <body className="antialiased">
+        <div
+          className="flex flex-col min-h-screen bg-cover bg-center bg-no-repeat"
+          style={{ backgroundImage: "url('/images/mesh_gradient.png')" }}
         >
-          <body className="antialiased">
-            <div className="flex flex-col min-h-screen bg-cover bg-center bg-no-repeat" style={{ backgroundImage: "url('/images/mesh_gradient.png')" }}>
-              <main className="min-w-0 flex flex-col items-center">
-                {/* <NavMenu /> */}
-                {children}
-                {/* <Footer /> */}
-                <Analytics />
-                <SpeedInsights />
-              </main>
-            </div>
-          </body>
-        </html>
-      );
-    }
+          <main className="flex flex-col items-center">
+            {/* <NavMenu /> */}
+            {children}
+            {/* <Footer /> */}
+            <Analytics />
+            <SpeedInsights />
+          </main>
+        </div>
+      </body>
+    </html>
+  );
+}
