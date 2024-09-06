e3067e9ea02b65631d54ca496e9487f250d951fb
Nicholas Tietje <tietje.n@northeastern.edu>
2024-09-06 08:25:45 -0400

    add viewport meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,6 +54,7 @@ export default function RootLayout({
       )}
     >
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body className="antialiased">
