f702a3ae7afb4d9efbad010faac12680938e12c7
Nick <tietje.n@northeastern.edu>
2024-09-07 17:04:50 -0400

    formatting configs

--- app/next.config.ts
+++ app/next.config.ts
@@ -2,14 +2,11 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   webpack: (config, { isServer }) => {
-    // Modify the optimization.minimize option
     config.optimization.minimize = false;
     config.optimization.minimizer = [];
 
-    // Return the modified config
     return config;
   },
-  // Other Next.js config options can go here
 };
 
 export default nextConfig;
