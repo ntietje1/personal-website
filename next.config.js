cb76d9988ae2dd537d3c3d0d095fd5d12e472e11
Nick <tietje.n@northeastern.edu>
2024-09-07 18:46:48 -0400

    adjust config + dependencies

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 const nextConfig = {
+  swcMinify: true,
   webpack: (config, { isServer }) => {
-    config.optimization.minimize = false;
-    config.optimization.minimizer = [];
+    // config.optimization.minimize = false;
+    // config.optimization.minimizer = [];
 
     return config;
   },
