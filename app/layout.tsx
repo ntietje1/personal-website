c31d75179181a9a513fd27bd9559418501a209bc
Nick <tietje.n@northeastern.edu>
2024-09-07 18:47:06 -0400

    remove unnecessary head

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,10 +53,10 @@ export default function RootLayout({
         GeistMono.variable
       )}
     >
-      <Head>
+      {/* <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-      </Head>
+      </Head> */}
       <body className="antialiased">
         <div
           className="flex flex-col min-h-screen bg-cover bg-center bg-no-repeat"
