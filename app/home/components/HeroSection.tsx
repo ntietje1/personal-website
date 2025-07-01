eca00dd3a6d51d13829ce7da8ef3ea65d9a704d1
Nicholas Tietje <tietje.n@northeastern.edu>
2025-06-30 20:39:31 -0400

    adjust text sizing and padding

--- app/home/components/HeroSection.tsx
+++ app/home/components/HeroSection.tsx
@@ -5,10 +5,10 @@ import Headshot from "./Headshot";
 function Name({ className }: { className?: string }) {
   return (
     <div className={`${className}`}>
-      <span className="text-lg md:text-2xl font-medium text-white pb-4 md:pb-8">
+      <span className="text-2xl font-medium text-white pb-8">
         Hi, my name is
       </span>
-      <h1 className="text-5xl md:text-7xl font-bold tracking-tight text-white md:mt-2 md:ml-[-5px]">
+      <h1 className="text-6xl font-bold tracking-tight text-white">
         Nick Tietje
       </h1>
     </div>
@@ -17,12 +17,12 @@ function Name({ className }: { className?: string }) {
 
 function Description({ className }: { className?: string }) {
   return (
-    <div className={`space-y-1 md:space-y-2 ${className}`}>
-      <h2 className="text-2xl md:text-3xl font-semibold text-white mt-4 md:mt-0">
+    <div className={`space-y-2 ${className}`}>
+      <h2 className="text-3xl font-semibold text-white mt-4">
         I'm a Software Engineer.
       </h2>
 
-      <p className="text-lg text-white max-w-lg">
+      <p className="text-lg font-medium text-white">
         I like to build full-stack web and mobile experiences.
       </p>
     </div>
@@ -32,30 +32,35 @@ function Description({ className }: { className?: string }) {
 export default function HeroSection() {
   return (
     <section className="min-h-screen flex flex-col justify-center relative">
-      <div className="flex flex-col items-center">
-        <div className="flex flex-col md:flex-row items-center justify-between w-full gap-32 md:gap-12">
+      <div className="flex flex-col items-center pb-4">
+        <div className="flex flex-col lg:flex-row items-center justify-between w-full gap-32 lg:gap-12">
           {/* Left content */}
-          <div className="flex flex-col items-center md:items-start text-center md:text-left flex-1 max-w-2xl">
+          <div className="flex flex-col items-center lg:items-start text-center lg:text-left flex-1 max-w-2xl">
             <div className="mb-8">
               <Name className="mb-4" />
-              <Description className="hidden md:block" />
+              <Description className="hidden lg:block" />
             </div>
 
             {/* Headshot */}
-            <Headshot className="md:hidden mt-8 h-64 w-64" imageOffset="5rem" />
+            <Headshot className="lg:hidden mt-8 h-64 w-64" imageOffset="5rem" />
 
-            <Description className="md:hidden mb-4" />
+            <Description className="lg:hidden mb-4" />
 
-            <div className="flex flex-col md:flex-row items-center gap-4 w-full">
-              <HoverableButton className="whitespace-nowrap" href="https://tietje-resume-bucket.s3.amazonaws.com/Nicholas_Tietje_Resume.pdf" target="_blank" rel="noopener noreferrer">
+            <div className="flex flex-col lg:flex-row items-center gap-4 w-full">
+              <HoverableButton
+                className="whitespace-nowrap"
+                href="https://tietje-resume-bucket.s3.amazonaws.com/Nicholas_Tietje_Resume.pdf"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 Download Resume
               </HoverableButton>
-              <ContactMenu className="flex gap-4 pl-0 md:pl-2" />
+              <ContactMenu className="flex gap-4 pl-0 lg:pl-2" />
             </div>
           </div>
 
           {/* Headshot (hidden on mobile) */}
-          <Headshot className="hidden md:block h-72 w-72" imageOffset="7rem" />
+          <Headshot className="hidden lg:block h-72 w-72" imageOffset="7rem" />
         </div>
       </div>
     </section>
